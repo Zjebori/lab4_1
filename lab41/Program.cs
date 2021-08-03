8444d44fc3b24538dd0060844b900de85c9d7ae1
Zaid Aljebori <zaidaljebori@rocketmortgage.com>
2021-08-03 11:11:04 -0400

    first commit12

---
+++ lab41/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace objectDemo2
+{
+    class Parallelogram
+    {
+        public double Length;
+        public double Width;
+
+        public double GetArea()
+        {
+            return Length * Width;
+
+        }
+
+        public double GetPerimeter()
+        {
+            return Length + Length + Width + Width;
+
+        }
+
+        public void print()
+        {
+            Console.WriteLine("here is your Parallelogram");
+            Console.WriteLine($"Length: {Length}, Width: {Width}");
+            Console.WriteLine($"Area: {GetArea()}, Perimeter : {GetPerimeter()}");
+        }
+
+        public void Resize(double NewLength, double Newwidth)
+        {
+            Length = NewLength;
+            Width = Newwidth;
+            
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parallelogram p1 = new Parallelogram() { Length = 10, Width = 20 };
+            Console.WriteLine(p1.GetArea());
+
+        }
+    }
+    
+}
